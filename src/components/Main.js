1775ff952681ca2e24fb631763384307d574c21c
Samuelisch <samuelchanwk@hotmail.com>
2021-11-19 18:28:35 +0800

    added styled form element, and section headers in form.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import SectionHeader from './assets/SectionHeader';
+
+const StyledContainer = styled.div`
+  border: 1px solid green;
+  width: 100%;
+  max-width: 650px;
+  margin: 50px;
+`;
+
+const StyledForm = styled.form`
+  padding: 20px;
+  .sectionHeader {
+    font-size: 2rem;
+  }
+`;
 
 const Main = () => {
   return (
-    <div>
-      Main Content
-    </div>
+    <StyledContainer className="mainContainer">
+      <StyledForm>
+        <SectionHeader className="info" text="General Info" />
+        <SectionHeader className="education" text="Education" />
+        <SectionHeader className="work" text="Work Experience" />
+      </StyledForm>
+    </StyledContainer>
   )
 }
 
