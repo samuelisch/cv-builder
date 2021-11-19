92bca311879d2f110c417fbb3b5b244f9cd39397
Samuelisch <samuelchanwk@hotmail.com>
2021-11-19 17:10:13 +0800

    added component for Header of webpage to include title of app.

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+  width: 100%;
+  background: lightblue;
+
+  h1 {
+    font-size: 3rem;
+    text-align: left;
+    padding-left: 10px;
+    margin: 15px;
+  }
+`;
+
+const Header = () => {
+  return (
+    <StyledHeader> 
+      <h1>CV creator</h1>
+    </StyledHeader>
+  )
+}
+
+export default Header;
