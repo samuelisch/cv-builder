4afc970dff06308a474c88afce16763f4d809863
Samuelisch <samuelchanwk@hotmail.com>
2021-11-20 20:57:12 +0800

    added styles to Input component

--- src/components/assets/Input.js
+++ src/components/assets/Input.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledContainer = styled.div``;
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 7px 0;
+`;
 
-const StyledInput = styled.input``;
+const StyledLabel = styled.label`
+  font-size: 1.4rem;
+`;
 
-const Input = ({ inputId, type, labelText, placeholderText, changeHandler, inputValue, required }) => {
+const StyledInput = styled.input`
+  width: 175px;
+`;
+
+const Input = ({ inputId, type, labelText, placeholderText, changeHandler, inputValue, required}) => {
   return (
     <StyledContainer>
-      <label htmlFor={inputId}>{labelText}</label>
+      <StyledLabel htmlFor={inputId}>{labelText}</StyledLabel>
       <StyledInput 
         type={type} 
         id={inputId} 
