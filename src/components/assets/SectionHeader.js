cea6d9bb6c85f251e500f4a79f7ac26670445b59
Samuelisch <samuelchanwk@hotmail.com>
2021-11-20 20:56:30 +0800

    added horizontal rule below section headers

--- src/components/assets/SectionHeader.js
+++ src/components/assets/SectionHeader.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledSectionHeader = styled.h2`
+  margin-bottom: 0;
+`;
+
+const StyledLine = styled.hr`
+  margin-top: 0;
+  border: 1px solid rgb(10, 10, 10);
+`;
 
 const SectionHeader = ({ className, text }) => {
   return (
-    <h2 className={`sectionHeader ${className}`}>{text}</h2>
+    <div>
+      <StyledSectionHeader className={`sectionHeader ${className}`}>{text}</StyledSectionHeader>
+      <StyledLine />
+    </div>
   )
 }
 
