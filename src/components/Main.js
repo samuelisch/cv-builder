7b344f9845acddf7512d275400f1ef53b60a08b7
Samuelisch <samuelchanwk@hotmail.com>
2021-11-20 20:58:31 +0800

    added state to Main component, to be used in form and cv parts of application

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import SectionHeader from './assets/SectionHeader';
 import ActionButtons from './sections/ActionButtons';
+import CVTemplate from './sections/EmptyCV';
+import View from './sections/View';
+import Info from './sections/Info';
+import Education from './sections/EduExp';
+import {v4 as uuidv4} from 'uuid';
+
+const StyledMain = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const StyledContainer = styled.div`
   border: 1px solid green;
-  width: 100%;
-  max-width: 650px;
+  width: 658px;
   margin: 50px;
 `;
 
@@ -18,15 +27,30 @@ const StyledForm = styled.form`
 `;
 
 const Main = () => {
+  const [cv, setCV] = useState(CVTemplate);
+
+  const downloadForm = (e) => {
+    e.preventDefault();
+    console.log('clicked download');
+  }
+
   return (
-    <StyledContainer className="mainContainer">
-      <StyledForm>
-        <SectionHeader className="info" text="General Info" />
-        <SectionHeader className="education" text="Education" />
-        <SectionHeader className="work" text="Work Experience" />
+    <StyledMain>
+      <StyledContainer className="mainContainer">
+        <StyledForm className="mainForm" onSubmit={downloadForm}>
+          <SectionHeader className="info" text="General Info" />
+          <Info />
+          <SectionHeader className="education" text="Education" />
+          <Education />
+          <SectionHeader className="work" text="Work Experience" />
+        </StyledForm>
+      </StyledContainer>
+
+      <StyledContainer>
+        <View />
         <ActionButtons />
-      </StyledForm>
-    </StyledContainer>
+      </StyledContainer>
+    </StyledMain>
   )
 }
 
