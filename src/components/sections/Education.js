0c42e45032837f7b84ce0b5819f027cf1222a2a9
Samuelisch <samuelchanwk@hotmail.com>
2021-11-21 15:13:40 +0800

    added buttons and handlers for adding instances of experiences.

--- src/components/sections/Education.js
+++ src/components/sections/Education.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import EduExp from './EduExp';
+import Button from '../assets/Button';
+import styled from 'styled-components';
 
-const Education = ({ changeHandler, education }) => {
+const StyledAddButton = styled(Button)`
+  font-size: 1.5rem;
+  background: rgb(120, 255, 120);
+  &:hover {
+    background: rgb(120, 240, 120);
+  }
+  margin: 10px;
+`;
+
+const StyledRemoveButton = styled(Button)`
+  font-size: 1.5rem;
+  background: rgb(255, 120, 120);
+  &:hover {
+    background: rgb(240, 120, 120);
+  }
+  margin: 10px;
+`;
+
+const Education = ({ changeHandler, education, addInstance, removeInstance }) => {
   const educationInstances = education.map(instance => (
     <EduExp
       key={instance.id}
@@ -14,6 +34,8 @@ const Education = ({ changeHandler, education }) => {
   return (
     <div>
       {educationInstances}
+      <StyledRemoveButton className="removeEducation" type="button" clickHandler={removeInstance} text="Remove" />
+      <StyledAddButton className="addEducation" type="button" clickHandler={addInstance} text="Add" />
     </div>
   )
 }
--- src/components/sections/Work.js
+++ src/components/sections/Work.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import WorkExp from './WorkExp';
+import Button from '../assets/Button'
+import styled from 'styled-components'
 
-const Work = ({ changeHandler, work }) => {
+const StyledAddButton = styled(Button)`
+  font-size: 1.5rem;
+  background: rgb(120, 255, 120);
+  &:hover {
+    background: rgb(120, 240, 120);
+  }
+  margin: 10px;
+`;
+
+const StyledRemoveButton = styled(Button)`
+  font-size: 1.5rem;
+  background: rgb(255, 120, 120);
+  &:hover {
+    background: rgb(240, 120, 120);
+  }
+  margin: 10px;
+`;
+
+const Work = ({ changeHandler, work, addInstance, removeInstance }) => {
   const workInstances = work.map(instance => (
     <WorkExp
       key={instance.id}
@@ -14,6 +34,8 @@ const Work = ({ changeHandler, work }) => {
   return (
     <div>
       {workInstances}
+      <StyledRemoveButton className="removeWork" type="button" clickHandler={removeInstance} text="Remove" />
+      <StyledAddButton className="addWork" type="button" clickHandler={addInstance} text="Add" />
     </div>
   )
 }
